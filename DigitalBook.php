a3b1670846c42fa1b13f3fee6c2b67be96ebe9ac
Alditia Muhamad Pirmansyah <alditiamp@gmail.com>
2026-09-02 19:48:06 +0700

    Added Digital Book

---
+++ DigitalBook.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'Book.php';
+
+class DigitalBook extends Book {
+    public $fileSize;
+    public $format;
+
+    public function __construct($judul, $penulis, $status, $fileSize, $format) {
+        parent::__construct($judul, $penulis, $status);
+        $this->fileSize = $fileSize;
+        $this->format = $format;
+    }
+
+    public function bookInfo() {
+        return parent::bookInfo() .
+               "Format: " . $this->format . "\n" .
+               "Ukuran File: " . $this->fileSize . " MB\n" . "\n";
+    }
+}
